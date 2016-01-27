a9167d48c9700085d4a738213a2526ce0d9474ad
geekgeek <solq87@gmail.com>
2016-01-28 00:40:54 +0100

    Create button_turn_on_led_2.c

---
+++ button_turn_on_led_2.c
@@ -0,0 +1,28 @@
+//pic12f683
+// https://www.youtube.com/watch?v=hkW5O5FCzMg
+// Button turn on LED
+//Binary converter: http://www.binaryconvert.com/convert_signed_char.html
+
+unsigned char i = 0;
+
+void main(void)
+{
+ TRISIO = 0b00000010; // TRISIO = 0x02;
+ CMCON0 = 0b00000111; //
+ ANSEL = 0;
+ 
+    while(1==1)
+    {
+     i = GPIO.B1;
+     
+     if(i == 0)
+     {
+      GPIO.B2 = 0;
+     }
+     if(i == 1)
+     {
+     GPIO.B2 = 1;
+     }
+     Delay_ms(500);
+    }
+}
